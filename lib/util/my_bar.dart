6dadaf6c7b091c99940690d667d339ee99eafa53
fahmialfaris <fahmialfaris1771@gmail.com>
2022-06-15 05:26:11 +0700

    test chart

--- lib/util/my_bar.dart
+++ lib/util/my_bar.dart
@@ -11,56 +11,105 @@ class MyBar extends StatelessWidget {
     return Expanded(
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 55, 220, 253),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-                width: 300.0,
-                alignment: Alignment.topCenter,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 30.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.favorite,
-                          color: Color.fromARGB(255, 32, 32, 32),
-                          size: 24.0,
-                        ),
-                      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 55, 220, 253),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
                     ),
-                    Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(7.0),
-                        child: Text(
-                          "You need to save 25K everyday",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Color.fromARGB(255, 34, 34, 34),
-                            fontWeight: FontWeight.normal,
+                  ),
+                  width: 300.0,
+                  alignment: Alignment.topCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 30.0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.favorite,
+                            color: Color.fromARGB(255, 32, 32, 32),
+                            size: 24.0,
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(7.0),
+                          child: Text(
+                            "You need to save 25K everyday",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 34, 34, 34),
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color.fromRGBO(183, 226, 196, 1),
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(183, 226, 196, 1),
+                        spreadRadius: 2,
+                        offset: Offset(0.0, 4.0),
+                      ),
+                    ],
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20, top: 25, bottom: 20),
+                              child: Container(
+                                height: 20,
+                                width: 10,
+                                color: Colors.amber,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 50, right: 20, top: 25, bottom: 20),
+                              child: Text(
+                                "6/2022",
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 23),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: 300,
-              width: 200,
-              color: Color.fromARGB(115, 165, 165, 165),
-            )
           ],
         ),
       ),
