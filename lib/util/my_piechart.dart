62864b22ea715c5e616595930173927c48654303
fahmialfaris <fahmialfaris1771@gmail.com>
2022-06-15 20:21:23 +0700

    fixing mybar

--- lib/util/my_piechart.dart
+++ lib/util/my_piechart.dart
@@ -12,11 +12,10 @@ class MyPieChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(32),
           child: PieChart(
             dataMap: dataMap,
             chartType: ChartType.ring,
@@ -29,7 +28,50 @@ class MyPieChart extends StatelessWidget {
             totalValue: 24000000,
           ),
         ),
-      ),
+        Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 55, 220, 253),
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          width: 260.0,
+          alignment: Alignment.topCenter,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 30.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.lightbulb_outline,
+                    color: Color.fromARGB(255, 32, 32, 32),
+                    size: 24.0,
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(7.0),
+                  child: Text(
+                    "You need to save 25K everyday",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 34, 34, 34),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        )
+      ],
     );
   }
 }
